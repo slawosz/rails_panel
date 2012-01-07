aabb0c390f7a33481c5d127714c300836112414e
Sławosz <slawosz@gmail.com>
2012-01-07 11:59:16 +0100

    use only helpers from current controller

--- lib/rails_panel/engine.rb
+++ lib/rails_panel/engine.rb
@@ -1,5 +1,9 @@
 module RailsPanel
   class Engine < Rails::Engine
     isolate_namespace RailsPanel
+
+    # cannot change this settings because RailsPanel based on
+    # overriding methods from RailsPanel::ResourceHelper in local helpers
+    config.action_controller.include_all_helpers = false
   end
 end
